e2f4dcbb236edda73772de9fb46cef73e3aee194
Asma <rababhina@hotmail.com>
2019-02-12 14:02:14 +0100

    first

---
+++ src/Api.test.js
@@ -0,0 +1,20 @@
+import ask from './Api';
+
+/*
+*
+* Tests, add the one listed test and minimum 2 more
+* -------------------------------------
+* - 'it should call YesNo API'
+* - *
+* - *
+* -------------------------------------
+*
+* Please add any other test that you think could be missing
+*
+* */
+
+
+describe('API Caller', () => {
+
+
+});
---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from './App';
+
+/*
+*
+* Tests, add the two listed test and minimum 3 more
+* -------------------------------------
+* - 'should display an input'
+* - 'should display a button'
+* - *
+* - *
+* - *
+* -------------------------------------
+*
+* */
